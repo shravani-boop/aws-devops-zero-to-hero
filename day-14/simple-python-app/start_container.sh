992de8c80b55144e02c9f26fb90afe17f5b98862
shravani-boop <shravanid046@gmail.com>
2026-07-07 14:50:21 +0530

    Update container management in start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# Stop old container if it exists
-docker rm -f simple-python-flask-app || true
+# Stop and remove old container 
+docker rm -f simple-python-flask-app:latest || true
 
 # Pull latest image
-docker pull shravanid123/simple-python-flask-app:latest
+docker pull shravanid123/simple-python-flask-app:latest 
 
-# Run container
+# Run new container
 docker run -d --name simple-python-flask-app -p 5000:5000 shravanid123/simple-python-flask-app:latest
