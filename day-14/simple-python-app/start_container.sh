ae1fde4567cfd4c79c6b302e9eb97e5dea7bfeb6
shravani-boop <shravanid046@gmail.com>
2026-07-07 14:27:03 +0530

    Update start_container.sh to pull latest image

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-docker push shravanid123/simple-python-flask-app:tagname
+# Stop old container if it exists
+docker rm -f simple-python-flask-app || true
 
-# Run the Docker image as a container
-docker run -d -p 5000:5000 shravanid123/simple-python-flask-app:tagname
+# Pull latest image
+docker pull shravanid123/simple-python-flask-app:latest
+
+# Run container
+docker run -d --name simple-python-flask-app -p 5000:5000 shravanid123/simple-python-flask-app:latest
